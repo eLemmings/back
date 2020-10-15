1743d935479237e0903746a542b79614248fa131
sadracco <sadracco@protonmail.com>
2020-10-15 23:37:55 +0200

    Usunięcie zbędnych plików
    Plik test.py był bezużyteczny

--- app/__init__.py
+++ app/__init__.py
@@ -14,11 +14,8 @@ app.config.update(config)
 api = Api(app)
 db = SQLAlchemy(app)
 
-from .test import test
 from app.utils import DbConnector
 
 db_connector = DbConnector(db)
 
 from app.api import *
-
-app.register_blueprint(test)
